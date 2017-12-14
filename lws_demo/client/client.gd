7609c8dc23ade1fe2c2a0e3735c0a9067b2c7016
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2017-12-14 19:37:35 +0100

    Allow setting write mode in lws demo

--- lws_demo/client/client.gd
+++ lws_demo/client/client.gd
@@ -6,12 +6,21 @@ var _client = WebSocketClient.new()
 var _peer = null
 var wsc = WebSocketPeer.new()
 var _connected = false
+var _write_mode = WebSocketPeer.WRITE_MODE_BINARY
 
 func _init():
 	_client.connect("connection_established", self, "_client_connected")
 	_client.connect("connection_error", self, "_client_disconnected")
 	_client.connect("connection_closed", self, "_client_disconnected")
 	_client.connect("data_received", self, "_client_received")
+
+func _ready():
+	var mode = get_node("../Panel/VBoxContainer/Send/Mode")
+	mode.clear()
+	mode.add_item("BINARY")
+	mode.set_item_metadata(0, WebSocketPeer.WRITE_MODE_BINARY)
+	mode.add_item("TEXT")
+	mode.set_item_metadata(1, WebSocketPeer.WRITE_MODE_TEXT)
 
 func _exit_tree():
 	_client.disconnect_from_host()
@@ -23,6 +32,7 @@ func _process(delta):
 func _client_connected(protocol):
 	Utils._log(_log_dest, "Client just connected with protocol: %s" % protocol)
 	_peer = Utils.Peer.new(_client.get_peer())
+	_peer.set_write_mode(_write_mode)
 
 func _client_disconnected():
 	Utils._log(_log_dest, "Client just disconnected")
@@ -31,7 +41,7 @@ func _client_disconnected():
 		_peer = null
 
 func _client_received():
-	Utils._log(_log_dest, "Client just received %s packets" % _peer._packet_peer.get_available_packet_count())
+	Utils._log(_log_dest, "Client just received %s packets" % _peer.get_available_packet_count())
 	var data = _peer.recv()
 	Utils._log(_log_dest, "Received data: %s" % data)
 
@@ -55,3 +65,8 @@ func _on_SendBtn_pressed():
 		_peer.send(txt)
 		get_node("../Panel/VBoxContainer/Send/LineEdit").set_text("")
 		Utils._log(_log_dest, "Sending data: %s" % txt)
+
+func _on_Mode_item_selected( ID ):
+	_write_mode = get_node("../Panel/VBoxContainer/Send/Mode").get_selected_metadata()
+	if _peer != null:
+		_peer.set_write_mode(_write_mode)
--- lws_demo/utils.gd
+++ lws_demo/utils.gd
@@ -1,30 +1,40 @@
 extends Node
 
-const PACKET = true
-
 class Peer extends Object:
 
 	var _packet_peer = null
+	var _write_mode = WebSocketPeer.WRITE_MODE_BINARY
 
 	func _init(peer=null):
 		_packet_peer = peer
 
 	func send(data):
-		if PACKET: # Sends as packets
-			_packet_peer.put_packet(var2bytes(data))
-		else: # Sends as var
+		if _write_mode == WebSocketPeer.WRITE_MODE_BINARY:
+			# Sends as Godot encoded variables
+			_packet_peer.put_var(data)
+		else:
+			# Sends as UTF-8 string
 			_packet_peer.put_packet(data.to_utf8())
 
 	func recv():
 		if _packet_peer.get_available_packet_count() == 0:
 			return null
-		if PACKET:
-				return bytes2var(_packet_peer.get_packet())
+		if _write_mode == WebSocketPeer.WRITE_MODE_BINARY:
+			# Receive as Godot encoded variables
+			return _packet_peer.get_var()
 		else:
-				return _packet_peer.get_packet().get_string_from_utf8()
+			# Receive as UTF-8 string
+			return _packet_peer.get_packet().get_string_from_utf8()
 
 	func get_available_packet_count():
 		return _packet_peer.get_available_packet_count()
+
+	func set_write_mode(mode):
+		_write_mode = mode
+		_packet_peer.set_write_mode(mode)
+
+	func is_binary_frame():
+		return _packet_peer.is_binary_frame()
 
 	func close():
 		_packet_peer.close()
